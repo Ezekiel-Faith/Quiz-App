6a727c3416523bdccaec4e392fc3cc3b0cc87188
Ezekie-Faith <ezekielfaith2000@gmail.com>
2025-04-08 17:40:59 +0100

    derived values and output questions from the data object

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,7 +1,30 @@
 import React, { useState } from 'react';
+import QUESTION from '../questions';
 
 export default function Quiz() {
-  const [activeQuestionIndex, setActiveQuestionIndex] = useState();
   const [userAnswer, setUserAnswers] = useState([]);
-  return <div>Quiz</div>;
+
+  const activeQuestionIndex = userAnswer.length;
+
+  function handleSelectAnswer(selectedAnswer) {
+    setUserAnswers((prevUserAnswer) => {
+      return [...prevUserAnswer, selectedAnswer];
+    });
+  }
+  return (
+    <div id='quiz'>
+      <div id='question'>
+        <h2>{QUESTION[activeQuestionIndex].text}</h2>
+        <ul id='answers'>
+          {QUESTION[activeQuestionIndex].answers.map((answer) => (
+            <li key={answer} className='answer'>
+              <button onClick={() => handleSelectAnswer(answer)}>
+                {answer}
+              </button>
+            </li>
+          ))}
+        </ul>
+      </div>
+    </div>
+  );
 }
