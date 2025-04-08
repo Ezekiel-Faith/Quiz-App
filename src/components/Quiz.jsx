f0daa43d645423addf58a904b939502d036acdac
Ezekie-Faith <ezekielfaith2000@gmail.com>
2025-04-08 17:05:34 +0100

    added the 'Header' and 'Quiz' comp

---
+++ src/components/Quiz.jsx
@@ -0,0 +1,7 @@
+import React, { useState } from 'react';
+
+export default function Quiz() {
+  const [activeQuestionIndex, setActiveQuestionIndex] = useState();
+  const [userAnswer, setUserAnswers] = useState([]);
+  return <div>Quiz</div>;
+}
