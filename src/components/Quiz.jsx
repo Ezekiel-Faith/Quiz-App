a0e1a27f5b3df65b9801f3ff5cf8f8698a340db3
Ezekie-Faith <ezekielfaith2000@gmail.com>
2025-04-08 17:56:48 +0100

    shuffled the annswers and added a quiz logic

--- src/components/Quiz.jsx
+++ src/components/Quiz.jsx
@@ -1,22 +1,38 @@
 import React, { useState } from 'react';
 import QUESTION from '../questions';
+import quizCompleteImg from '../assets/quiz-complete.png';
 
 export default function Quiz() {
   const [userAnswer, setUserAnswers] = useState([]);
 
   const activeQuestionIndex = userAnswer.length;
 
+  const quizIsComplete = activeQuestionIndex === QUESTION.length;
+
   function handleSelectAnswer(selectedAnswer) {
     setUserAnswers((prevUserAnswer) => {
       return [...prevUserAnswer, selectedAnswer];
     });
   }
+
+  if (quizIsComplete) {
+    return (
+      <div id='summary'>
+        <img src={quizCompleteImg} alt='Trophy Icon' />
+        <h2>Quiz Completed!</h2>
+      </div>
+    );
+  }
+
+  const shuffledAnswers = [...QUESTION[activeQuestionIndex].answers];
+  shuffledAnswers.sort(() => Math.random() - 0.5);
+
   return (
     <div id='quiz'>
       <div id='question'>
         <h2>{QUESTION[activeQuestionIndex].text}</h2>
         <ul id='answers'>
-          {QUESTION[activeQuestionIndex].answers.map((answer) => (
+          {shuffledAnswers.map((answer) => (
             <li key={answer} className='answer'>
               <button onClick={() => handleSelectAnswer(answer)}>
                 {answer}
